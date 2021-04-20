dde83350bb1220b120b865822383746b0478315d
DeniCarvalho <denii.carvalho94@gmail.com>
2021-04-20 20:50:20 -0300

    gtag install

--- src/main.js
+++ src/main.js
@@ -18,6 +18,11 @@ import store from './store'
 // import './plugins/vue-firebase'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'AW-765565322' },
+})
 
 Vue.config.productionTip = false
 new Vue({
